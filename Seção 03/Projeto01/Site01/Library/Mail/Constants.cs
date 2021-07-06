fe245b511f9961c3e322a717d2e598509c3cab20
Maurício Martins <maur.martins32@gmaail.com>
2021-07-06 11:51:30 -0300

    Projeto Finalizado

---
+++ Seção 03/Projeto01/Site01/Library/Mail/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Library.Mail
+{
+    public class Constants
+    {
+        /**
+         * POP3, IMAP - Ler mensagens de e-mail
+         * SMTP - Enviar e-mail
+         */
+
+        //Autenticação - Gmail
+        public readonly static string Usuario = "maur.martins32@gmail.com";
+        public readonly static string Senha = Environment.GetEnvironmentVariable("senha");
+
+        //Servidor SMTP
+        public readonly static string ServidorSMTP = "smtp.gmail.com";
+        public readonly static int PortaSMTP = 587;
+    }
+}
