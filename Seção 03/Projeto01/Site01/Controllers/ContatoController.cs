01343eff993d6601f2701335b277e215402a97d2
Maurício Martins <maur.martins32@gmaail.com>
2021-07-10 14:57:39 -0300

    Adicionado validações do lado do cliente e servidor

--- Seção 03/Projeto01/Site01/Controllers/ContatoController.cs
+++ Seção 03/Projeto01/Site01/Controllers/ContatoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Site01.Library.Mail;
+using System.Text.RegularExpressions;
 using Site01.Models;
 
 namespace Site01.Controllers
@@ -17,25 +18,92 @@ namespace Site01.Controllers
         }
 
 
+        [HttpPost]
         public IActionResult ReceberContato([FromForm] Contato contato)
         {
-            if (ModelState.IsValid)
+            if (string.IsNullOrEmpty(contato.Nome))
             {
-                //string conteudo = string.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
-                //return new ContentResult() { Content = conteudo };
+                ViewBag.Erro = "O campo 'Nome' é obrigatório!";
+                ViewBag.Contato = contato;
 
-                ViewBag.Contato = new Contato();
-                EnviarEmail.EnviarMensagemContato(contato);
-                ViewBag.Mensagem = "Mensagem enviada com sucesso!";
                 return View("Index");
             }
-            else
+            else if (contato.Nome.Length > 50)
             {
+                ViewBag.Erro = "O campo 'Nome' deve conter no máximo 50 caracteres!";
+                ViewBag.Contato = contato;
+
+                return View("Index");
+            }
+            else if (string.IsNullOrEmpty(contato.Email))
+            {
+                ViewBag.Erro = "O campo 'E-mail' é obrigatório!";
+                ViewBag.Contato = contato;
+
+                return View("Index");
+            }
+            else if (contato.Email.Length > 70)
+            {
+                ViewBag.Erro = "O campo 'E-mail' deve conter no máximo 70 caracteres!";
+                ViewBag.Contato = contato;
+
+                return View("Index");
+            }
+            else if (string.IsNullOrEmpty(contato.Assunto))
+            {
+                ViewBag.Erro = "O campo 'Assunto' é obrigatório!";
+                ViewBag.Contato = contato;
+
+                return View("Index");
+            }
+            else if (contato.Assunto.Length > 70)
+            {
+                ViewBag.Erro = "O campo 'Assunto' deve conter no máximo 70 caracteres!";
+                ViewBag.Contato = contato;
+
+                return View("Index");
+            }
+            else if (string.IsNullOrEmpty(contato.Mensagem))
+            {
+                ViewBag.Erro = "O campo 'Mensagem' é obrigatório!";
+                ViewBag.Contato = contato;
+
+                return View("Index");
+            }
+            else if (contato.Mensagem.Length > 2000)
+            {
+                ViewBag.Erro = "O campo 'Mensagem' deve conter no máximo 2000 caracteres!";
                 ViewBag.Contato = contato;
 
                 return View("Index");
             }
 
+            else
+            {
+                ViewBag.Contato = new Contato();
+                EnviarEmail.EnviarMensagemContato(contato);
+                ViewBag.Mensagem = "Mensagem enviada com sucesso!";
+                return View("Index");
+            }
+
+
+            /* if (ModelState.IsValid)
+             {
+                 string conteudo = string.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+                 return new ContentResult() { Content = conteudo };
+
+                 ViewBag.Contato = new Contato();
+                 EnviarEmail.EnviarMensagemContato(contato);
+                 ViewBag.Mensagem = "Mensagem enviada com sucesso!";
+                 return View("Index");
+             }
+             else
+             {
+                 ViewBag.Contato = contato;
+
+                 return View("Index");
+             }
+            */
 
         }
 
---
+++ Seção 03/Projeto01/Site01/wwwroot/Scripts/Validation.js
@@ -0,0 +1,53 @@
+﻿const form = document.querySelector("form")
+const nome = document.querySelector("input[name='nome']")
+const email = document.querySelector("input[name='email']")
+const assunto = document.querySelector("input[name='assunto']")
+const mensagem = document.querySelector("textarea")
+const alertMessage = document.querySelector(".alert")
+
+alertMessage.style.display = "none"
+
+form.onsubmit = function (e) {
+    if (nome.value.trim() === "") {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Nome' é obrigatório!"
+    } else if (nome.value.length > 50) {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Nome' deve conter no máximo 50 caracteres!"
+    } else if (email.value.trim() === "") {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Email' é obrigatório!"
+    } else if (email.value.length > 70) {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Nome' deve conter no máximo 70 caracteres!"
+    }
+    else if (!isEmail(email.value)) {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'E-mail' é inválido!"
+    } else if (assunto.value.trim() === "") {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Assunto' é obrigatório!"
+    } else if (assunto.value.length > 70) {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Assunto' deve conter no máximo 70 caracteres!"
+    } else if (mensagem.value.trim() === "") {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Mensagem' é obrigatório!"
+    } else if (mensagem.value.length > 2000) {
+        e.preventDefault()
+        alertMessage.style.display = "block"
+        alertMessage.innerText = "O campo 'Mensagem' deve conter no máximo 2000 caracteres!"
+    }
+}
+
+function isEmail(email) {
+    return /^[^\s@]+@[^\s@]+$/.test(email)
+}
